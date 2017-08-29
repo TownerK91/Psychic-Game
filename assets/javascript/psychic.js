d8eff4282c257e9a6891cb925e553295ca559f26
Kevin Towner <kevin91@Kevins-MacBook-Pro.local>
2017-08-28 21:40:12 -0400

    save

---
+++ assets/javascript/psychic.js
@@ -0,0 +1,71 @@
+//create a variable for a random letter
+alert("I have yet to figure out how to make the html responsive to the javascript. So for now, I will make the game with many alerts.")
+
+var answer = 3;
+var guess = prompt("Choose a number");
+
+
+
+//-----------------------//
+//--//--//--	//--//--TEST PRACTICE ECT--//
+//-----------------------//
+
+for (i = 0; i < 10; i++){
+    console.log(i + 1);
+    		if(answer == guess){
+    			alert("You've earned 1 point on your 'wins:' record");
+    			break;
+    		}else{ 
+    			guess = prompt ("please try again");
+
+
+
+    			alert("You've lost 1 point on your 'losses:' record");
+    		}
+ }  		
+    		
+
+
+
+
+    			//---add 1 point to the Wins:
+
+
+    			//--------------------------------//
+    		
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//create html doc with the fields for:
+// -->Wins
+
+
+// -->Losses
+
+
+//-->Guesses Left/10
+
+
+//--->Guesses so far
+
+
+
+//code to recognize when the random letter was chosen
+
+
+
+//if this right letter was guessed, plus by 1
+
+
+//Find a way to RESET everything when guesses hits 10
